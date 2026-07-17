f0199617ab58600383b492d918bbba16ed66a1fb
Bobby Ho <poke@bobbyho.me>
2026-07-17 20:32:31 +0800

    Implement text copy function

--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from collections.abc import Callable, Sequence
 from pathlib import Path
 
-from gi.repository import Adw, GObject, Gtk
+from gi.repository import Adw, Gdk, GLib, GObject, Gtk
 
 from src.ocr.ocr import Image, Text
 from src.ui.ImageTextOverlay import ImageTextOverlay
@@ -27,7 +27,9 @@ class MainWindow(Adw.ApplicationWindow):
     __gtype_name__ = "MainWindow"
 
     _overlay_container: Adw.Bin = Gtk.Template.Child("overlay_container")
-    selected_text: SelectedText = Gtk.Template.Child("selected_text")
+    _toast_overlay: Adw.ToastOverlay = Gtk.Template.Child("toast-overlay")
+    _selected_text: SelectedText = Gtk.Template.Child("selected_text")
+    _btn_copy_text: Gtk.Button = Gtk.Template.Child("btn-copy-text")
 
     def __init__(
         self,
@@ -48,6 +50,23 @@ class MainWindow(Adw.ApplicationWindow):
 
     def _handle_text_clicked(self, text: Text) -> None:
         "Handle a click on a text bounding box by updating the selected text property."
-        self.selected_text.props.text = text.text
-        self.selected_text.props.score = f"{text.score:.3f}"
-        print(f"Selected text: {text.text} (score: {text.score:.2f})")
+        self._selected_text.props.text = text.text
+        self._selected_text.props.score = f"{text.score:.3f}"
+        print(f"Selected text: {text.text} (score: {text.score:.3f})")
+
+        # When clicking on a text, activate the copy button
+        self._btn_copy_text.activate()
+
+    @Gtk.Template.Callback()
+    def on_copy_text_clicked(self, _button: Gtk.Button) -> None:
+        """Handle a click on the copy text button"""
+        content = Gdk.ContentProvider.new_for_bytes(
+            "text/plain;charset=utf-8",
+            GLib.Bytes.new(self._selected_text.props.text.encode("utf-8")),
+        )
+        self.get_display().get_clipboard().set_content(content)
+
+        # Notify the text has been copied
+        toast = Adw.Toast.new("Text copied to clipboard")
+        toast.set_timeout(1)
+        self._toast_overlay.add_toast(toast)
--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,12 +1,12 @@
-from .ImageTextOverlay import ImageTextOverlay
-from .MainWindow import MainWindow
-from .App import App
-
 import gi
 
 gi.require_version("Adw", "1")
 gi.require_version("Gdk", "4.0")
 gi.require_version("Graphene", "1.0")
 gi.require_version("Gtk", "4.0")
+
+from .ImageTextOverlay import ImageTextOverlay # noqa: E402
+from .MainWindow import MainWindow # noqa: E402
+from .App import App # noqa: E402
 
 __all__ = ["ImageTextOverlay", "MainWindow", "App"]
